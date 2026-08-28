a5bc74acfcc4f61136ce22ea58dad610d70939f0
Artur Czuba <artur.czuba.94@gmail.com>
2026-08-28 21:33:48 +0200

    JUS-78 Add heatmap recorder

--- docs/analytics.test.cjs
+++ docs/analytics.test.cjs
@@ -3,13 +3,17 @@ const fs = require("node:fs");
 const path = require("node:path");
 const test = require("node:test");
 
-const analyticsScript =
-  '<script defer src="https://statystyki.justcode.ac/script.js" data-website-id="adb5be85-1f83-44a8-a215-6954adf36a93"></script>';
+const analyticsScripts = [
+  '<script defer src="https://statystyki.justcode.ac/script.js" data-website-id="adb5be85-1f83-44a8-a215-6954adf36a93"></script>',
+  '<script defer src="https://statystyki.justcode.ac/recorder.js" data-website-id="adb5be85-1f83-44a8-a215-6954adf36a93"></script>',
+];
 
 for (const page of ["index.html", "pl/index.html"]) {
-  test(`${page} loads the JustCode analytics script exactly once`, () => {
+  test(`${page} loads each JustCode analytics script exactly once`, () => {
     const html = fs.readFileSync(path.join(__dirname, page), "utf8");
 
-    assert.equal(html.split(analyticsScript).length - 1, 1);
+    for (const analyticsScript of analyticsScripts) {
+      assert.equal(html.split(analyticsScript).length - 1, 1);
+    }
   });
 }
