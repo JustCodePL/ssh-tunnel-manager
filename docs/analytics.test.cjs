20eef7566c0631a6a5295b3fc8fa3aa6a1479481
Artur Czuba <artur.czuba.94@gmail.com>
2026-08-28 21:28:53 +0200

    JUS-78 Add site analytics

---
+++ docs/analytics.test.cjs
@@ -0,0 +1,15 @@
+const assert = require("node:assert/strict");
+const fs = require("node:fs");
+const path = require("node:path");
+const test = require("node:test");
+
+const analyticsScript =
+  '<script defer src="https://statystyki.justcode.ac/script.js" data-website-id="adb5be85-1f83-44a8-a215-6954adf36a93"></script>';
+
+for (const page of ["index.html", "pl/index.html"]) {
+  test(`${page} loads the JustCode analytics script exactly once`, () => {
+    const html = fs.readFileSync(path.join(__dirname, page), "utf8");
+
+    assert.equal(html.split(analyticsScript).length - 1, 1);
+  });
+}
